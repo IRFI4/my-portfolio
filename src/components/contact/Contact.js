eaf866a5299873dd75cb7f70a69c5fce40427805
IRFI4 <vladisglyadelkin@gmail.com>
2024-07-16 23:06:27 +0300

    fixed form value

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,26 +1,40 @@
 import React from 'react'
 import Icon from '../ui/icon/Icon'
+import emailjs from 'emailjs-com'
+
 const Contact = () => {
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs.sendForm('service_cw8fmmo', 'template_6z4zuuk', e.target, 'QjI65UGkSHIE7D01g')
+            .then((result) => {
+                console.log(result.text)
+            }, (error) => {
+                console.log(error.text)
+            })
+    }
+
     return (
         <section id='contact' className='contact-wrapper'>
             <div className='contact-left'>
-                <form className='contact-form' action="mailto:vladisglyadelkin@gmail.com" method="post" enctype="text/plain">
+                <form className='contact-form' onSubmit={sendEmail}>
                     <h4 className='sub-title'><span>Let's work together</span></h4>
                     <p className='text'>
                         I'm always excited to connect with new people and
                         explore opportunities for collaboration.
                     </p>
-                    <input type='text' placeholder='First name' />
-                    <input type='text' placeholder='Last name ' />
-                    <input type='email' placeholder='Email address' />
-                    <input type='phone' placeholder='Phone number' />
-                    <select>
-                        <option>Select a service</option>
-                        <option>Web Development</option>
-                        <option>UI/UX Design</option>
-                        <option>Logo Design</option>
+                    <input type='text' name='first_name' placeholder='First name' required />
+                    <input type='text' name='last_name' placeholder='Last name' required />
+                    <input type='email' name='email_address' placeholder='Email address' required />
+                    <input type='phone' name='phone_number' placeholder='Phone number' required />
+                    <select name="select_service" required>
+                        <option value=''>Select a service</option>
+                        <option value='web_development'>Web Development</option>
+                        <option value='ui_ux_design'>UI/UX Design</option>
+                        <option value='logo_design'>Logo Design</option>
                     </select>
-                    <textarea placeholder='Type your message here.'></textarea>
+                    <textarea name='message' placeholder='Type your message here.' required></textarea>
                     <button type='submit'>Send message</button>
                 </form>
             </div>
